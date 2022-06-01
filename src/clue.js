46ae30c196586ee1c863ec69cc4e62f98b6ae674
MAG-91 <mag_91@hotmail.de>
2022-06-01 17:51:54 +0200

    update

--- src/clue.js
+++ src/clue.js
@@ -1,30 +1,188 @@
 // ITERATION 1
 
 // Suspects Array
+// {objekte} in [arrays]
+const suspectsArray = [
+  {
+    firstName: 'Jacob',
+    lastName: 'Green',
+    occupation: 'Entrepreneur',
+    age: 45,
+    description: 'He has a lot of connections',
+    image:
+      'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+    color: 'green'
+  },
 
-const suspectsArray = [];
+  {
+    firstName: 'Doctor',
+    lastName: 'Orchid',
+    occupation: 'Scientist',
+    age: 26,
+    description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+    image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    color: 'white'
+  },
 
+  {
+    firstName: 'Victor',
+    lastName: 'Plum',
+    occupation: 'Designer',
+    age: 22,
+    description: 'Billionaire video game designer',
+    image:
+      'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
+    color: 'purple'
+  },
+
+  {
+    firstName: 'Kasandra',
+    lastName: 'Scarlet',
+    occupation: 'Actor',
+    age: 31,
+    description: 'She is an A-list movie star with a dark past',
+    image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    color: 'red'
+  },
+
+  {
+    firstName: 'Eleanor',
+    lastName: 'Peacock',
+    occupation: 'Socialité',
+    age: 36,
+    description:
+      'She is from a wealthy family and uses her status and money to earn popularity',
+    image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+    color: 'blue'
+  },
+
+  {
+    firstName: 'Jack',
+    lastName: 'Mustard',
+    occupation: 'Retired Football player',
+    age: 62,
+    description:
+      'He is a former football player who tries to get by on his former glory',
+    image:
+      'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+    color: 'yellow'
+  }
+];
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+  { name: 'Dining Room' },
+  { name: 'Conservatory' },
+  { name: 'Kitchen' },
+  { name: 'Study' },
+  { name: 'Library' },
+  { name: 'Billiard Room' },
+  { name: 'Lounge' },
+  { name: 'Ballroom' },
+  { name: 'Hall' },
+  { name: 'Spa' },
+  { name: 'Living Room' },
+  { name: 'Observatory' },
+  { name: 'Theater' },
+  { name: 'Guest House' },
+  { name: 'Patio' }
+];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [
+  {
+    name: 'rope',
+    weight: 1
+  },
 
+  {
+    name: 'knife',
+    weight: 8
+  },
+
+  {
+    name: 'candlestick',
+    weight: 2
+  },
+
+  {
+    name: 'dumbbel',
+    weight: 3
+  },
+
+  {
+    name: 'poison',
+    weight: 2
+  },
+
+  {
+    name: 'axe',
+    weight: 1
+  },
+
+  {
+    name: 'bat',
+    weight: 1
+  },
+
+  {
+    name: 'trophy',
+    weight: 2
+  },
+
+  {
+    name: 'pistol',
+    weight: 20
+  }
+];
 
 // ITERATION 2
+/*
+function selectRandom() {
+  const cards = [];
+  for (let w = 0; w < weaponsArray.length; w++) {
+    for (let s = 0; s < suspectsArray.length; s++) {
+      for (let r = 0; r < roomsArray.length; r++) {
+        const weapon = weaponsArray[w];
+        const suspect = suspectsArray[s];
+        const room = roomsArray[r];
+        cards.push({weapon, suspect, room});
+      }
+    }
+  }
+return cards;
+}
+console.log(selectRandom());
+*/
 
-function selectRandom() {}
+function selectRandom(cards) {
+  let deck = cards.length;
 
-function pickMystery() {}
+  let rndCard = Math.floor(Math.random() * deck);
+  return cards[rndCard];
+}
 
+function pickMystery() {
+  let obj = {};
+  obj.weapons = selectRandom(weaponsArray);
+  obj.suspect = selectRandom(suspectsArray);
+  obj.room = selectRandom(roomsArray);
+  return obj;
+}
+
+//console.log(pickMystery());
 
 // ITERATION 3
 
-function revealMystery() {}
+function revealMystery() {
+  let mystery = pickMystery;
+  tfirstname = mystery.suspect;
+  //
+  //
 
-
+  return 'fir';
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
